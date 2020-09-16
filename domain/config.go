00a4b8fda64b543dc5ed1b038d94152f7a586d51
goforbroke1006 <go.for.broke1006@gmail.com>
2020-09-16 19:52:42 +0300

    fix config reading

--- domain/config.go
+++ domain/config.go
@@ -5,11 +5,13 @@ type Configuration struct {
 }
 
 type Active struct {
-	Code      string
-	ValueOpts struct {
-		Start         float64
-		Upper         float64
-		Bottom        float64
-		VolatilityAbs float64
-	}
+	Code string
+	Opts ValueOpts
+}
+
+type ValueOpts struct {
+	Start         float64
+	Upper         float64
+	Bottom        float64
+	VolatilityAbs float64
 }
